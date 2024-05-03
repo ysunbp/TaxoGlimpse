6578ce8d0e0d92b8c6d8fc07cdada3dcaeff6a5b
Yushi Sun <41725714+ysunbp@users.noreply.github.com>
2024-05-03 19:03:59 +0800

    Create ebay_generate_question_pool.py

---
+++ LLM-taxonomy/shopping/scripts/ebay_generate_question_pool.py
@@ -0,0 +1,142 @@
+from bs4 import BeautifulSoup
+import csv
+import random
+from tqdm import tqdm
+
+
+# 读取 HTML 文件
+with open('eBay.html', 'r') as file:
+    html_content = file.read()
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+links = soup.find_all('a')
+
+child2parent_dict_1 = {}
+parent2child_dict_1 = {}
+
+child2parent_dict_2 = {}
+parent2child_dict_2 = {}
+
+root_set = []
+
+level_1 = []
+
+level_2 = []
+for link in links:
+    if "top-cat" in str(link):
+        h2_tag = link.find('h2')
+        if h2_tag:
+            text = h2_tag.text
+            root_set.append(text.strip())
+            cur_parent = text.strip()
+    elif "cat-img-wrapper all-cats-lazy-load" in str(link):
+        if 'Top Brands' in str(link) or 'Top Stores' in str(link) or 'Popular Topics' in str(link):
+            continue
+        img_tag = link.find('img')
+        if img_tag:
+            alt_value = img_tag.get('alt')
+            level_1_node = alt_value.strip()
+            if level_1_node in child2parent_dict_1.keys():
+                child2parent_dict_1[level_1_node].append(cur_parent)
+            level_1.append(level_1_node)
+            child2parent_dict_1[level_1_node] = [cur_parent]
+            if not cur_parent in parent2child_dict_1.keys():
+                parent2child_dict_1[cur_parent] = [level_1_node]
+            else:
+                parent2child_dict_1[cur_parent].append(level_1_node)
+            cur_sub_parent = level_1_node
+    elif "title" in str(link) and "href=\"https://www.ebay.com/b/" in str(link):
+        if 'Top Brands' in str(link) or 'Top Stores' in str(link) or 'Popular Topics' in str(link):
+            continue
+        title_value = link.get('title')
+        if title_value:
+            level_2_node = title_value.strip()
+            if level_2_node in child2parent_dict_2.keys():
+                #print(level_2_node, '|', cur_sub_parent)
+                child2parent_dict_2[level_2_node].append(cur_sub_parent)
+            level_2.append(level_2_node)
+            child2parent_dict_2[level_2_node] = [cur_sub_parent]
+            if not cur_sub_parent in parent2child_dict_2.keys():
+                parent2child_dict_2[cur_sub_parent] = [level_2_node]
+            else:
+                parent2child_dict_2[cur_sub_parent].append(level_2_node)
+
+print(len(list(set(level_1))))
+
+print(len(list(set(level_2))))
+
+level_nodes = [root_set, level_1, level_2]
+
+def get_cur_level_uncles(cur_level_nodes, level_nodes, level):
+    uncle_dict = {}
+    if level == 1:
+        for node in cur_level_nodes:
+            uncle_dict[node] = list(set(level_nodes[0])-set(child2parent_dict_1[node]))
+        return uncle_dict
+    else:
+        for node in cur_level_nodes:
+            cur_parent_node = child2parent_dict_2[node][0]
+            cur_grand_node = child2parent_dict_1[cur_parent_node][0]
+            cur_uncles = parent2child_dict_1[cur_grand_node]
+            if len(cur_uncles) > len(child2parent_dict_2[node]):
+                uncle_dict[node] = list(set(cur_uncles)-set(child2parent_dict_2[node]))
+            else:
+                uncle_dict[node] = list(set(level_nodes[level-1])-set(child2parent_dict_2[node]))
+        return uncle_dict
+
+def sample_question_pool(cur_level_nodes, level_nodes, nodes_uncles_dict, sample_size, cur_level, out_path, question_mode=0):
+    with open(out_path, 'a', newline='') as file:
+        csv_writer = csv.writer(file)
+        if question_mode == 0:
+            if len(cur_level_nodes) < sample_size:
+                sampled_nodes = cur_level_nodes
+            else:
+                sampled_nodes = random.sample(cur_level_nodes, sample_size)
+            for node in tqdm(sampled_nodes):
+                if cur_level == 1:
+                    cur_template = ('shopping-ebay', child2parent_dict_1[node][0], node, cur_level, 'level-positive')
+                else:
+                    cur_template = ('shopping-ebay', child2parent_dict_2[node][0], node, cur_level, 'level-positive')
+                csv_writer.writerow(cur_template)
+            print('size of the positive set', len(sampled_nodes), cur_level)
+        elif question_mode == 1: # negative hard question
+            if len(cur_level_nodes) < sample_size:
+                sampled_nodes = cur_level_nodes
+            else:
+                sampled_nodes = random.sample(cur_level_nodes, sample_size)
+            
+            for node in tqdm(sampled_nodes):
+                cur_template = ('shopping-ebay', random.choice(nodes_uncles_dict[node]), node, cur_level, 'level-negative-hard')
+                csv_writer.writerow(cur_template)
+            print('size of the negative set', len(sampled_nodes), cur_level)
+
+        elif question_mode == 2:
+            if len(cur_level_nodes) < sample_size:
+                sampled_nodes = cur_level_nodes
+            else:
+                sampled_nodes = random.sample(cur_level_nodes, sample_size)
+            
+            for node in tqdm(sampled_nodes):
+                if cur_level == 1:
+                    cur_template = ('shopping-ebay', random.choice(list(set(level_nodes[cur_level-1])-set(child2parent_dict_1[node]))), node, cur_level, 'level-negative-easy')
+                else:
+                    cur_template = ('shopping-ebay', random.choice(list(set(level_nodes[cur_level-1])-set(child2parent_dict_2[node]))), node, cur_level, 'level-negative-easy')
+                csv_writer.writerow(cur_template)
+            print('size of the negative set', len(sampled_nodes), cur_level)
+
+
+num_question_samples = [100000, 100000]
+
+file_name = ['positive.csv', 'negative_hard.csv', 'negative_easy.csv']
+
+
+for cur_question_mode in range(3):
+    out_path = 'TaxoGlimpse/question_pools/shopping-ebay/level/question_pool_full_' + file_name[cur_question_mode]
+    with open(out_path, 'a', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['domain', 'parent', 'child', 'level', 'type'])  # 写入表头
+    
+    for cur_level in range(1, len(num_question_samples)+1):
+        nodes_uncles_dict = get_cur_level_uncles(level_nodes[cur_level], level_nodes, cur_level) # level 1是从level 1 到root
+        sample_question_pool(level_nodes[cur_level], level_nodes, nodes_uncles_dict, sample_size=num_question_samples[cur_level-1], cur_level=cur_level, out_path=out_path, question_mode=cur_question_mode)
